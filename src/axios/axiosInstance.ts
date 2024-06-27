b1909149380ed399948ac6a4b2e80d51bdfa6611
hdev1004 <h.dev@kakao.com>
2024-06-28 00:04:29 +0900

    fix: axiosInstance 수정

--- src/axios/axiosInstance.ts
+++ src/axios/axiosInstance.ts
@@ -3,11 +3,7 @@ import axios from 'axios'
 
 const AxiosInstance = axios.create({
   baseURL: '/', // 기본 URL 설정
-  timeout: 10000, // 요청이 타임아웃되기까지의 시간 (ms)
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization: 'Bearer your_token_here' // 필요에 따라 헤더 추가
-  }
+  timeout: 10000 // 요청이 타임아웃되기까지의 시간 (ms)
 })
 
 //요청 인터셉터
